24d622c69f3332297b57826abe14b56354de038b
BamanGirma <amanuelgirma360@gmail.com>
2024-08-09 10:57:08 +0300

    building react home page

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from '../src/Header/Header'
+import Alert from '../src/Alert/Alert'
+import FirstSection from './FirstSection/FirstSection'
+import SecondSection from './SecondSection/SecondSection'
+import ThridSection from './ThridSection/ThridSection'
+import FourthSection from './FourthSection/FourthSection'
+import FifthSection from './FifthSection/FifthSection'
+import SixthSection from './SixthSection/SixthSection'
+import Footer from './Footer/Footer'
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+     <Header/>
+     <Alert/>
+     <FirstSection/>
+     <SecondSection/>
+     <ThridSection/>
+     <FourthSection/>
+     <FifthSection/>
+     <SixthSection/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
